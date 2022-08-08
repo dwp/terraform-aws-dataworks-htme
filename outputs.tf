ec4e5bb9c8ed6292f611b13e424be4e0aa993c49
rorybryett <rory.bryett@dwp.gov.uk>
2022-08-08 19:17:49 +0100

    sg dependency violation - test fix

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "sg_id" {
-    value = aws_security_group.htme.id
-    description = "HTME security group ID."
+  value       = aws_security_group.htme.id
+  description = "HTME security group ID."
 }
 
 output "asg_name" {
-    value = aws_autoscaling_group.htme.name
-    description = "HTME Auto-scaling group name."
+  value       = aws_autoscaling_group.htme.name
+  description = "HTME Auto-scaling group name."
 }
--- sg.tf
+++ sg.tf
@@ -1,7 +1,11 @@
 resource "aws_security_group" "htme" {
-  name        = "htme"
+  name_prefix = "htme"
   description = "Control access to and from the hbase-to-mongo-exporter Hosts"
   vpc_id      = var.vpc_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = merge(
     var.common_tags,
