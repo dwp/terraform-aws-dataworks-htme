f95bbd2824993e9939acee47e15ab77bb7b350de
rorybryett <rory.bryett@dwp.gov.uk>
2022-08-08 19:50:27 +0100

    sg dependency violation - test fix

--- sg.tf
+++ sg.tf
@@ -1,7 +1,8 @@
 resource "aws_security_group" "htme" {
-  name_prefix = "htme"
-  description = "Control access to and from the hbase-to-mongo-exporter Hosts"
-  vpc_id      = var.vpc_id
+  name_prefix            = "htme"
+  description            = "Control access to and from the hbase-to-mongo-exporter Hosts"
+  revoke_rules_on_delete = true
+  vpc_id                 = var.vpc_id
 
   lifecycle {
     create_before_destroy = true
