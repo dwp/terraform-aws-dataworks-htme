ffed226b922196ce57a3c7fa80fd647343a1f5cf
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-03-14 08:24:12 +0000

    chore: removed duplicate resources from config

--- example/data.tf
+++ example/data.tf
@@ -13,4 +13,3 @@ data "local_file" "logging_script" {
 data "local_file" "htme_default_topics_ris_csv" {
   filename = "files/htme/htme_default_topics_ris/htme_default_topics_ris_development.csv"
 }
-
