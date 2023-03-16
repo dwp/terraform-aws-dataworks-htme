57ebd959a83a58f1697fdd988eaf2b368644c096
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-03-16 16:23:00 +0000

    chore: Fixing deprectaions

--- example/s3.tf
+++ example/s3.tf
@@ -44,61 +44,81 @@ resource "aws_s3_bucket" "compaction" {
     },
   )
 
-  versioning {
-    enabled = false
-  }
+}
 
-  logging {
-    target_bucket = data.terraform_remote_state.security_tools.outputs.logstore_bucket.id
-    target_prefix = "S3Logs/terratest-compaction-bucket/ServerLogs"
-  }
 
-  lifecycle_rule {
-    id      = "DeleteNonCurrentVersion"
-    prefix  = ""
-    enabled = true
-
-    noncurrent_version_expiration {
-      days = 1
+resource "aws_s3_bucket_lifecycle_configuration" "compaction" {
+  bucket = aws_s3_bucket.compaction.id
+  rule {
+    id = "DeleteNonCurrentVersion"
+    filter {
+      prefix = ""
     }
+    noncurrent_version_expiration {
+      noncurrent_days = 1
+    }
+
+    status = "Enabled"
   }
 
-  lifecycle_rule {
-    id      = "DeleteSnapshotSender"
-    prefix  = "businessdata/mongo/snapshot-sender-status/"
-    enabled = true
-
+  rule {
+    id = "DeleteSnapshotSender"
+    filter {
+      prefix = "businessdata/mongo/snapshot-sender-status/"
+    }
     expiration {
       days = 2
     }
+
+    status = "Enabled"
   }
 
-  lifecycle_rule {
-    id      = "DeleteCryptoDataAfterOneDay"
-    prefix  = "businessdata/mongo/ucdata/*/*/db.crypto"
-    enabled = true
-
+  rule {
+    id = "DeleteCryptoDataAfterOneDay"
+    filter {
+      prefix = "businessdata/mongo/ucdata/*/*/db.crypto"
+    }
     expiration {
       days = 1
     }
+
+    status = "Enabled"
   }
 
-  lifecycle_rule {
-    id      = "DeleteOldSnapshots"
-    prefix  = "${var.htme_s3_prefix}/"
-    enabled = true
-
+  rule {
+    id = "DeleteOldSnapshots"
+    filter {
+      prefix = "${var.htme_s3_prefix}/"
+    }
     expiration {
       days = 7
     }
-  }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.compaction_bucket_cmk.arn
-        sse_algorithm     = "aws:kms"
-      }
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "compaction" {
+  bucket = aws_s3_bucket.compaction.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_logging" "compaction" {
+  bucket = aws_s3_bucket.compaction.id
+
+  target_bucket = data.terraform_remote_state.security_tools.outputs.logstore_bucket.id
+  target_prefix = "S3Logs/terratest-compaction-bucket/ServerLogs"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "compaction" {
+  bucket = aws_s3_bucket.compaction.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.compaction_bucket_cmk.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
@@ -188,61 +208,129 @@ resource "aws_s3_bucket" "manifest_bucket" {
     },
   )
 
-  versioning {
-    enabled = false
-  }
+}
 
-  logging {
-    target_bucket = data.terraform_remote_state.security_tools.outputs.logstore_bucket.id
-    target_prefix = "S3Logs/terratest-manifest-bucket/ServerLogs"
-  }
 
-  lifecycle_rule {
-    id      = local.manifest_s3_bucket_streaming_manifest_lifecycle_rule_name_main
-    prefix  = "${data.terraform_remote_state.ingestion.outputs.k2hb_manifest_write_locations.main_prefix}/"
-    enabled = true
-
+resource "aws_s3_bucket_lifecycle_configuration" "manifest_bucket" {
+  bucket = aws_s3_bucket.manifest_bucket.id
+  rule {
+    id = local.manifest_s3_bucket_streaming_manifest_lifecycle_rule_name_main
+    filter {
+      prefix = "${data.terraform_remote_state.ingestion.outputs.k2hb_manifest_write_locations.main_prefix}/"
+    }
     expiration {
       days = 2
     }
+
+    status = "Enabled"
   }
 
-  lifecycle_rule {
-    id      = local.manifest_s3_bucket_streaming_manifest_lifecycle_rule_name_equality
-    prefix  = "${data.terraform_remote_state.ingestion.outputs.k2hb_manifest_write_locations.equality_prefix}/"
-    enabled = true
-
+  rule {
+    id = local.manifest_s3_bucket_streaming_manifest_lifecycle_rule_name_equality
+    filter {
+      prefix = "${data.terraform_remote_state.ingestion.outputs.k2hb_manifest_write_locations.equality_prefix}/"
+    }
     expiration {
       days = 2
     }
+
+    status = "Enabled"
   }
 
-  lifecycle_rule {
-    id      = local.manifest_s3_bucket_streaming_manifest_lifecycle_rule_name_audit
-    prefix  = "${data.terraform_remote_state.ingestion.outputs.k2hb_manifest_write_locations.audit_prefix}/"
-    enabled = true
-
+  rule {
+    id = local.manifest_s3_bucket_streaming_manifest_lifecycle_rule_name_audit
+    filter {
+      prefix = "${data.terraform_remote_state.ingestion.outputs.k2hb_manifest_write_locations.audit_prefix}/"
+    }
     expiration {
       days = 2
     }
+
+    status = "Enabled"
   }
 
-  lifecycle_rule {
-    id      = local.manifest_s3_bucket_export_manifest_lifecycle_rule_name
-    prefix  = "${local.htme_s3_manifest_prefix}/"
-    enabled = true
-
+  rule {
+    id = local.manifest_s3_bucket_export_manifest_lifecycle_rule_name
+    filter {
+      prefix = "${local.htme_s3_manifest_prefix}/"
+    }
     expiration {
       days = 2
     }
+
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "manifest_bucket" {
+  bucket = aws_s3_bucket.manifest_bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_logging" "manifest_bucket" {
+  bucket = aws_s3_bucket.manifest_bucket.id
+
+  target_bucket = data.terraform_remote_state.security_tools.outputs.logstore_bucket.id
+  target_prefix = "S3Logs/terratest-manifest-bucket/ServerLogs"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "manifest_bucket" {
+  bucket = aws_s3_bucket.manifest_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.manifest_bucket_cmk.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "compaction" {
+  bucket = aws_s3_bucket.compaction.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+data "aws_iam_policy_document" "compaction_bucket" {
+  statement {
+    sid     = "BlockHTTP"
+    effect  = "Deny"
+    actions = ["*"]
+
+    resources = [
+      aws_s3_bucket.compaction.arn,
+      "${aws_s3_bucket.compaction.arn}/*",
+    ]
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    condition {
+      test     = "Bool"
+      values   = ["false"]
+      variable = "aws:SecureTransport"
+    }
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.manifest_bucket_cmk.arn
-        sse_algorithm     = "aws:kms"
-      }
+  statement {
+    sid     = "DenyGetObjectOnCryptoCollectionsToAllExceptDataEgressRole"
+    effect  = "Deny"
+    actions = ["s3:GetObject"]
+
+    resources = [
+      "${aws_s3_bucket.compaction.arn}/businessdata/mongo/ucdata/*/*/db.crypto",
+    ]
+
+    not_principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.account[local.environment]}:role/DataEgressServer"]
     }
   }
 }
