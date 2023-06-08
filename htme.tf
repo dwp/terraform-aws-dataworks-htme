74a31834ecd2b4af295d8c425841c48e270fe22e
jonnym <jon.melia@cloudadept.co.uk>
2023-06-08 10:04:17 +0100

    Update htme.tf (#45)
    Signed-off-by: jonnym <jon.melia@cloudadept.co.uk>

--- htme.tf
+++ htme.tf
@@ -387,7 +387,6 @@ data "template_file" "htme_fallback" {
     tenantid         = var.tenantid
     token            = var.token
     policyid         = var.policyid
-    tanium_prefix    = var.tanium_prefix
   }
 }
 
